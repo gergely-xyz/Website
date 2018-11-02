8b20af729073f2fc3e4c19859fd94efa795b02ac
horvathge <ghorvath@femtonics.eu>
2018-11-02 21:42:57 +0100

    Update Kaleidoscope demo

--- projects/kaleidoscope_demo/sketch.js
+++ projects/kaleidoscope_demo/sketch.js
@@ -1,37 +1,38 @@
-class Blob{
-    constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.R = 0;
-        this.G = 0;
-        this.B = 0;
+class Blob {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.R = 0;
+    this.G = 0;
+    this.B = 0;
 
-        this.d = 20;
+    this.d = 20;
+  }
 
-    }
+  draw() {
+    fill(this.R, this.G, this.B);
+    ellipse(this.x, this.y, this.d, this.d);
+  }
 
-    draw(){
-        fill(this.R, this.G, this.B);
-        ellipse(this.x, this.y, this.d, this.d)
-    }
-    
-    moveTo(x,y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    setColor(R,G,B){
-        this.R = R;
-        this.G = G;
-        this.B = B;
-    }
+  moveTo(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    setDiameter(d){
-        this.d = d;
-    }
+  setColor(R, G, B) {
+    this.R = R;
+    this.G = G;
+    this.B = B;
+  }
+
+  setDiameter(d) {
+    this.d = d;
+  }
 }
 
-var blobs = [new Blob(), new Blob(), new Blob(), new Blob(), new Blob(), new Blob(), new Blob(), new Blob()];
+var cnv;
+
+var blobs = [];
 
 var myX = 0;
 var myY = 0;
@@ -51,67 +52,75 @@ var myblue = 0;
 var blueshift = 1;
 
 function setup() {
-    // Begin drawing in the center
-    myX = width/2;
-    myY = height/2;
+  // Make 8 blobs
+  for (var i = 0; i < 8; i++) {
+    blobs.push(new Blob());
+  }
 
-    // Make canvas in the center of the window
-    var cnv = createCanvas(800,800);
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 2;
-    cnv.position(x, y);
+  // Make canvas in the center of the window
+  cnv = createCanvas(canvasSize(), canvasSize());
+  centerCanvas();
 
-    noStroke();
-    background(0);
+  noStroke();
 }
 
-
 function draw() {
-    // Get a random position
-    myX = noise(xoff)*width;
-    myY = noise(yoff)*height;
+  // Get a random position
+  myX = noise(xoff) * width;
+  myY = noise(yoff) * height;
 
-    xoff = xoff + 0.005;
-    yoff = yoff + 0.004;
+  xoff = xoff + 0.005;
+  yoff = yoff + 0.004;
 
-    // Set random diameter 5-25
-    myD = noise(doff)*20+5;
-    doff = doff + 0.006;
+  // Set random diameter 5-25
+  myD = noise(doff) * 20 + 5;
+  doff = doff + 0.006;
 
-    // Move blobs to position
-    blobs[0].moveTo(myX, myY);
-    blobs[1].moveTo(width-myX, myY);
-    blobs[2].moveTo(myX, height-myY);
-    blobs[3].moveTo(width-myX, height-myY);
-    
-    blobs[4].moveTo(myY, myX);
-    blobs[5].moveTo(width-myY, myX);
-    blobs[6].moveTo(myY, height-myX);
-    blobs[7].moveTo(width-myY, height-myX);
+  // Move blobs to position
+  blobs[0].moveTo(myX, myY);
+  blobs[1].moveTo(width - myX, myY);
+  blobs[2].moveTo(myX, height - myY);
+  blobs[3].moveTo(width - myX, height - myY);
 
-    // Shift color back and forth
-    myred = myred+redshift;
-    mygreen = mygreen+greenshift;
-    myblue = myblue+blueshift;
+  blobs[4].moveTo(myY, myX);
+  blobs[5].moveTo(width - myY, myX);
+  blobs[6].moveTo(myY, height - myX);
+  blobs[7].moveTo(width - myY, height - myX);
 
-    if (myred >= 255 || myred <= 0){
-        redshift = -1*redshift;
-    }
+  // Shift color back and forth
+  myred = myred + redshift;
+  mygreen = mygreen + greenshift;
+  myblue = myblue + blueshift;
 
-    if (myblue >= 255 || myblue <= 0){
-        blueshift = -1*blueshift;
-    }
+  if (myred >= 255 || myred <= 0) {
+    redshift = -1 * redshift;
+  }
 
-    if (mygreen >= 255 || mygreen <= 0){
-        greenshift = -1*greenshift;
-    }
+  if (myblue >= 255 || myblue <= 0) {
+    blueshift = -1 * blueshift;
+  }
 
-    // Draw with the blobs
-    for (var i = 0; i<8; i++){
-        blobs[i].setDiameter(myD);
-        blobs[i].setColor(myred, mygreen, myblue);
-        blobs[i].draw();
-    }
+  if (mygreen >= 255 || mygreen <= 0) {
+    greenshift = -1 * greenshift;
+  }
 
-    
+  // Draw with the blobs
+  for (var i = 0; i < 8; i++) {
+    blobs[i].setDiameter(myD);
+    blobs[i].setColor(myred, mygreen, myblue);
+    blobs[i].draw();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(canvasSize(), canvasSize());
+  centerCanvas();
+}
+
+function centerCanvas() {
+  cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+}
+
+function canvasSize() {
+  return min(windowWidth, windowHeight);
 }
